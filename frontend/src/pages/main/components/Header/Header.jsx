c3cab752951d81eed72228034c4af270bf03c606
choi hyo jin <dkdlelrkdlrj@naver.com>
2025-08-12 12:12:35 +0900

    ✨ [#4] MainPage-header - Register

--- frontend/src/pages/main/components/Header/Header.jsx
+++ frontend/src/pages/main/components/Header/Header.jsx
@@ -1,23 +1,43 @@
+import { useNavigate } from 'react-router';
 import './Header.css';
 
+
+
 export default function Header() {
+const navigate = useNavigate();
+
+// 회원가입
+const register = () => {
+  navigate("/Register");
+};
+
+
+
+
+
     return (
-        <header className="header">
-            <div className="nav">
-                <div className="logo">DeVim</div>
+      <header className="header">
+        <div className="nav">
+          <div className="logo">DeVim</div>
 
-                <nav className="menu">
-                    <a href="#">Home</a>
-                    <a href="#">게시판</a>
-                </nav>
+          <nav className="menu">
+            <a href="#">Q&A 게시판</a>
+            <a href="#">게시판</a>
+          </nav>
 
-                <input type="text" className="search" placeholder="검색어를 입력해주세요" />
+          <input
+            type="text"
+            className="search"
+            placeholder="검색어를 입력해주세요"
+          />
 
-                <div className="auth">
-                    <button className="btn btn--sign">로그인</button>
-                    <button className="btn btn--register">회원가입</button>
-                </div>
-            </div>
-        </header>
+          <div className="auth">
+            <button className="btn btn--sign">로그인</button>
+            <button className="btn btn--register" onClick={register}>
+              회원가입
+            </button>
+          </div>
+        </div>
+      </header>
     );
 }
--- frontend/src/router/root.jsx
+++ frontend/src/router/root.jsx
@@ -4,26 +4,32 @@ import { Suspense } from "react";
 import { lazy } from "react";
 
 const MainPage = lazy(() => import('../pages/main/MainPage'));
+const Register = lazy(() => import("../pages/SignUpRegister/Register"));
 
 
 const root = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout />,
-        children: [
-            {
-                path: 'main',
-                element: (
-                    <Suspense>
-                        <MainPage />
-                    </Suspense>
-                )
-            },
-
-
-
-        ],
-    }
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "main",
+        element: (
+          <Suspense>
+            <MainPage />
+          </Suspense>
+        ),
+      },
+      {
+        path: "register",
+        element: (
+          <Suspense>
+            <Register />
+          </Suspense>
+        ),
+      },
+    ],
+  },
 ]);
 
 export default root;
