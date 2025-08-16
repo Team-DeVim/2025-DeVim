f61db20375844915c48612be4c3fa6d8cc72a2fa
Woojin CHO <littletree0330@gmail.com>
2025-08-16 17:33:21 +0900

    ✨ [#31]Impliment Complelte

---
+++ frontend/src/pages/boardPage/components/NotionSection/NotionSection.jsx
@@ -0,0 +1,11 @@
+import "./NotionSection.css";
+
+const NotionSection = () => {
+    return (
+        <div className="NotionSection">
+            NotionSection
+        </div>
+    );
+};
+
+export default NotionSection;
