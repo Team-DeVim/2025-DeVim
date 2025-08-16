99b68041ae1e97f5b85fb5c0f765cb725626196a
Trajectory-SH <trajectory0611@gmail.com>
2025-08-16 22:29:26 +0900

    ✨ [#30]BoardController & Service

---
+++ backend/src/main/java/com/Devim/backend/service/board/BoardServieImpl.java
@@ -0,0 +1,57 @@
+package com.Devim.backend.service.board;
+
+import com.Devim.backend.domain.board.Board;
+import com.Devim.backend.domain.board.BoardDto;
+import com.Devim.backend.domain.common.PageRequestDto;
+import com.Devim.backend.domain.common.PageResponseDto;
+import com.Devim.backend.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BoardServieImpl implements BoardService {
+
+    private final BoardRepository boardRepository;
+
+
+    @Override
+    @Transactional
+    public Long create(Board board) {
+        boardRepository.save(board);
+        return 0L;
+    }
+
+    @Override
+    public Optional<BoardDto> get(Long boardNo) {
+        return boardRepository.findById(boardNo);
+    }
+
+    @Override
+    public PageResponseDto<BoardDto> list(String title, PageRequestDto pageRequestDto) {
+        return boardRepository.findAll(pageRequestDto, title);
+    }
+
+    @Override
+    public List<BoardDto> listPopular(int limit) {
+        return boardRepository.findPopularBoards(limit);
+    }
+
+    @Override
+    @Transactional
+    public void update(Board board) {
+        boardRepository.update(board);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long boardNo) {
+        boardRepository.deleteById(boardNo);
+    }
+}
---
+++ backend/src/main/java/com/Devim/backend/service/comment/CommentService.java
@@ -0,0 +1,18 @@
+package com.Devim.backend.service.comment;
+
+import com.Devim.backend.domain.comment.Comment;
+import com.Devim.backend.domain.comment.CommentDto;
+import com.Devim.backend.domain.common.PageRequestDto;
+import com.Devim.backend.domain.common.PageResponseDto;
+
+public interface CommentService {
+    Long create(Comment comment);
+
+    CommentDto get(Long commentNo);
+
+    PageResponseDto<CommentDto> listByBoard(Long boardNo, PageRequestDto pageRequestDto);
+
+    void update(Comment comment);
+
+    void delete(Comment commentNo);
+}
