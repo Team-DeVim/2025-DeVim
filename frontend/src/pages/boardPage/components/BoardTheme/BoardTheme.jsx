f73f802178dd3d670b0aff20b23d44ccd9d9f416
Woojin CHO <littletree0330@gmail.com>
2025-08-22 19:31:06 +0900

    ✨ [#79] Impliment Notice detailPage

--- frontend/src/pages/boardPage/components/BoardTheme/BoardTheme.jsx
+++ frontend/src/pages/boardPage/components/BoardTheme/BoardTheme.jsx
@@ -2,11 +2,24 @@ import React from "react";
 import "./BoardTheme.css";
 
 export default function BoardTheme({ boardTypeNo }) {
-  const boardTitle =
-    (boardTypeNo === 1 ? "자유게시판" : "Q&A 게시판");
+  let boardTitle = "";
+  if (boardTypeNo === 1) {
+    boardTitle = "자유게시판";
+  } else if (boardTypeNo === 2) {
+    boardTitle = "Q&A 게시판";
+  } else {
+    boardTitle = "공지사항";
+  }
 
-  const boardSubtitle =
-    (boardTypeNo === 1 ? "다양한 주제로 생각을 넓혀보세요" : "각 분야 코딩러들에게 힘을 빌려보세요");
+  let boardSubtitle = "";
+  if (boardTypeNo === 1) {
+    boardSubtitle = "다양한 주제로 생각을 넓혀보세요"
+  } else if (boardTypeNo === 2) {
+    boardSubtitle = "각 분야 코딩러들에게 힘을 빌려보세요";
+  } else {
+    boardSubtitle = "Devim에서 알려드립니다";
+  }
+
   return (
     <section className="boardTheme">
       <div className="boardTheme__titleRow">
--- frontend/src/pages/boardPage/components/NoticeSection/NoticeSection.jsx
+++ frontend/src/pages/boardPage/components/NoticeSection/NoticeSection.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { createSearchParams, Link } from "react-router-dom";
 import "./NoticeSection.css";
 
 export default function NoticeSection({ data }) {
   const noticeList = data
+  console.log(noticeList);
   return (
     <section className="noticeSection">
       <ul className="noticeSection__list">
         {noticeList.map((n) => (
           <li key={n.boardNo} className="noticeSection__item">
-            <Link className="noticeSection__link" to={`/notice/${n.boardNo}`}>
+            <Link className="noticeSection__link"
+              to={{
+                pathname: `/detailPage/${n.boardNo}`,
+                search: `?${createSearchParams({
+                  boardTypeNo: "3",
+                })}`,
+              }}
+            >
               <span className="noticeSection__badge">{"공지사항"}</span>
               <span className="noticeSection__text">{n.title}</span>
             </Link>
