d1cfc927510f47031e827b190a798d4f3e11fdce
parkkangho <etzz@naver.com>
2025-08-26 17:53:30 +0900

    jwt swagger permitAll

--- backend/src/main/java/com/Devim/backend/config/security/SecurityConfig.java
+++ backend/src/main/java/com/Devim/backend/config/security/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/login",   "/", "/sign-up").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
 
                         .anyRequest().authenticated());
