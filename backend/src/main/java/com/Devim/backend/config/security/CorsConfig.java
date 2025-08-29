77b403a0626f86774fef22c3b0207958455ede16
parkkangho <etzz@naver.com>
2025-08-29 17:54:30 +0900

    🐛 [#141] 사용자 프로필 수정 시 이미지 경로 예외처리

--- backend/src/main/java/com/Devim/backend/config/security/CorsConfig.java
+++ backend/src/main/java/com/Devim/backend/config/security/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173") // Vite dev 서버 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
